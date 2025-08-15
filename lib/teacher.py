e933f2ae272b0e512ca70040071252675d6f6d93
fidel-otieno2 <fidelotieno05@gmail.com>
2025-08-15 13:01:41 +0300

    complete

--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 
 from user import User
-
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = [
+            "str is a data type in Python",
+            "a Python function can return itself",
+            "Python is an interpreted language",
+            "Python is dynamically typed",
+            "Python supports multiple inheritance"
+        ]
 
     def teach(self):
-        pass
+        return random.choice(self.knowledge)
